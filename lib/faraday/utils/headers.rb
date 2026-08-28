d64cfcaefa99bb74392f688a622aa2a07de4df5f
Oskar Eichler <62393985+OskarEichler@users.noreply.github.com>
2026-08-28 14:13:01 +0300

    Copy header names before retaining them in the case index

--- lib/faraday/utils/headers.rb
+++ lib/faraday/utils/headers.rb
@@ -56,7 +56,7 @@ module Faraday
 
       def []=(key, val)
         key = KeyMap[key]
-        key = (@names[key.downcase] ||= key)
+        key = (@names[key.downcase] ||= key.dup.freeze)
         # join multiple values with a comma
         val = val.to_ary.join(', ') if val.respond_to?(:to_ary)
         super
